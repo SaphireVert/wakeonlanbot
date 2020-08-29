af8f83660298192cee4d42fe2015541520dc74bc
saphirevert <jerome.cosandey@gmail.com>
2020-08-29 18:48:26 +0200

    New files and tests

--- index.js
+++ index.js
@@ -3,6 +3,28 @@ const token = require('./secrets.json');
 const BOT_TOKEN = token.BOT_TOKEN;
 const bot = new TeleBot(BOT_TOKEN);
 
-bot.on('text', (msg) => msg.reply.text(msg.text));
+// const utils = require('./utils.js');
+
+
+
+bot.on('/wol', (msg) => {
+    msg.reply.text(msg.text);
+    console.log(msg);
+    if(msg.from.id == 976140946){
+      console.log("Yeah, la bonne personne !");
+      const exec = require('child_process').exec;
+      const myShellScript = exec('sh wol_PC.sh');
+      myShellScript.stdout.on('data', (data)=>{
+          console.log(data);
+          // do whatever you want here with data
+      });
+      myShellScript.stderr.on('data', (data)=>{
+          console.error(data);
+      });
+    } else {
+      console.log("Merde ! Un intrus !");
+    }
+});
+// bot.on('/wol', (msg) => msg.reply.text(msg));
 
 bot.start();
